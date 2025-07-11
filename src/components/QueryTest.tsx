ffa0d9cdc72da09b925e66de6ead6fb65e270d16
shinwokkang <tlsdnr303@gmail.com>
2025-07-11 11:39:06 +0900

    feat: 임시 컴포넌트 생성 (pages/home/Homepage.tsx 에서 사용하는데 Component 가 없어서 임의로 생성함)

---
+++ src/components/QueryTest.tsx
@@ -0,0 +1,10 @@
+const QueryTest = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">테스트용</h2>
+      <p>테스트 Component.</p>
+    </div>
+  );
+};
+
+export default QueryTest;
