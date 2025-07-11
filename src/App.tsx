0cdac4f4211a240fd28b81c3d612b69f3e2b249b
shinwokkang <tlsdnr303@gmail.com>
2025-07-11 11:41:24 +0900

    feat: App.tsx의 기본 path를 SplashPage 로 전환

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import HomePage from "@/pages/home/Homepage";
+// import Homepage from "./pages/home/Homepage";
+import SplashPage from "@/pages/splash/SplashPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<SplashPage />} />
       </Routes>
     </BrowserRouter>
   );
