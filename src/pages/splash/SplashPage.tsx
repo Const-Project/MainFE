2856ae2b5f6ee70a9e319f04fb5729d8d5cfe156
shinwokkang <tlsdnr303@gmail.com>
2025-07-11 12:52:40 +0900

    fix: 디바이스 별 폰트 사이즈 적용을 위한 px 단위에서 vw 사용

--- src/pages/splash/SplashPage.tsx
+++ src/pages/splash/SplashPage.tsx
@@ -1,15 +1,15 @@
 const SplashPage = () => {
   return (
-    <div className="min-h-screen bg-white flex justify-center items-center">
-      <div className="w-[394px] h-[852px] bg-white  flex flex-col relative">
-        <div className="flex-grow flex justify-center items-center">
-          <h1 className="text-[210%] font-bold text-black">나풀나풀</h1>
-        </div>
-        <div className="absolute bottom-10 w-full">
-          <p className="text-sm text-black text-center">
-            슬로건입니다. 더미텍스트입니다.
-          </p>
-        </div>
+    <div className="w-full h-screen bg-white flex flex-col relative">
+      <div className="flex-grow flex justify-center items-center">
+        <h1 className="text-[9vw] md:text-8xl font-bold text-black">
+          나풀나풀
+        </h1>
+      </div>
+      <div className="absolute bottom-10 w-full">
+        <p className="text-[4vw] md:text-2xl text-gray-500 text-center">
+          슬로건입니다. 더미텍스트입니다.
+        </p>
       </div>
     </div>
   );
