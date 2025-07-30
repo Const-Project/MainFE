cb6c0ea842341e00102f0eeed2e408a7b23271d9
shinwokkang <tlsdnr303@gmail.com>
2025-07-30 21:34:40 +0900

    feat: 아바타 생성 페이지에서 사용하는 생성 및 만들기 UI 구현

---
+++ src/components/registration/AvatarCreationOption.tsx
@@ -0,0 +1,27 @@
+/**
+ * @file AvatarCreationOption.tsx
+ * @description 아바타 선택 페이지의 옵션 컴포넌트
+ */
+
+import React from "react";
+
+const AvatarCreationOption = () => {
+  return (
+    <div className="flex items-center justify-between h-full">
+      <div className="flex flex-col justify-between pl-6.25 h-full">
+        <div className="flex flex-col gap-3 w-38.75">
+          <h2 className="text-2xl font-semibold pt-8">아바타 선택</h2>
+          <p className="text-lg ">00종의 아바타 중에서 선택할 수 있어요</p>
+        </div>
+        <div className="pb-10.25">
+          <button className=" bg-primary w-34.5 h-11.25 text-white rounded-lg">
+            선택하러 가기
+          </button>
+        </div>
+      </div>
+      <div className="w-1/2 h-full "></div>
+    </div>
+  );
+};
+
+export default AvatarCreationOption;
