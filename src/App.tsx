aca09ae784218ccb23008ab558bcaedb4d4c19c5
shinwokkang <tlsdnr303@gmail.com>
2025-07-30 21:35:22 +0900

    feat: 아바타 생성 페이지 구현 및 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SplashPage from "@/pages/splash/SplashPage";
 import Design from "@/components/common/Design";
 
+import AvatarPage from "./pages/registration/AvatarPage";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -11,6 +13,7 @@ const App = () => {
         {/* <Route path="/" element={<HomePage />} /> */}
         <Route path="/" element={<SplashPage />} />
         <Route path="/design" element={<Design />} />
+        <Route path="/registration/avatar" element={<AvatarPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/registration/AvatarPage.tsx
@@ -0,0 +1,31 @@
+/**
+ * @file AvatarPage.tsx
+ * @description 아바타 선택 페이지
+ */
+
+import React from "react";
+
+import AvatarCreationOption from "../../components/registration/AvatarCreationOption";
+import AvatarSelectionOption from "../../components/registration/AvatarSelectionOption";
+import ButtonFooter from "../../components/registration/common/ButtonFooter";
+import RegistrationHeader from "../../components/registration/common/RegistrationHeader";
+
+const AvatarPage = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <RegistrationHeader />
+      <main className="flex-grow flex flex-col min-h-0">
+        <div className="flex-1">
+          <AvatarCreationOption />
+        </div>
+
+        <div className="flex-1">
+          <AvatarSelectionOption />
+        </div>
+      </main>
+      <ButtonFooter />
+    </div>
+  );
+};
+
+export default AvatarPage;
