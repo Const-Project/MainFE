d695fe408b26a28dc0a6ddcbffc62c5f055bb379
shinwokkang <tlsdnr303@gmail.com>
2025-07-31 13:26:56 +0900

    feat: Button.tsx에 Creation, Selection 에서 사용하는 스타일 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -6,15 +6,22 @@
 
 import React from "react";
 
+/**
+ * @description ButtonProps 인터페이스는 버튼 컴포넌트의 속성을 정의합니다.
+ * @property {string} [variant] - 버튼의 배경 색, 텍스트 색상을 지정합니다. 기본값은 'primary'입니다.
+ * @property {string} [size] - 버튼의 크기를 지정합니다. 기본값은 'sm'입니다.
+ * @property {React.ReactNode} children - 버튼 내부에 렌더링할 내용을 지정합니다.
+ * @property {React.ButtonHTMLAttributes<HTMLButtonElement>} [className] - 추가적인 CSS 클래스를 지정할 수 있습니다.
+ */
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary";
-  size?: "sm" | "lg"; // 'sm'은 기존 버튼, 'lg'는 새로운 큰 버튼
+  variant?: "primary" | "gray200" | "gray600";
+  size?: "xsSelect" | "xsCreation" | "sm" | "lg"; // 'sm'은 기존 버튼, 'lg'는 새로운 큰 버튼
   children: React.ReactNode;
 }
 
 const Button: React.FC<ButtonProps> = ({
   variant = "primary",
-  size = "sm", // 기본값 'sm'으로 설정
+  size = "sm",
   children,
   className,
   ...props
@@ -22,13 +29,16 @@ const Button: React.FC<ButtonProps> = ({
   const baseStyles = "flex items-center justify-center text-center rounded "; // 공통 스타일
 
   const sizeStyles = {
-    sm: "w-42.5 h-full py-3.5 text-[20px]", // 기존 버튼 크기 및 폰트 사이즈
-    lg: "w-[353px] h-[57px] text-[20px]", // 새로운 큰 버튼 크기 및 폰트 사이즈
+    xsSelect: "w-34.5 h-11.25 text-[18px] px-5 py-2",
+    xsCreation: "w-30.75 h-11.25 text-[18px] px-5 py-2 ",
+    sm: "w-42.5 h-full py-3.5 text-[20px]",
+    lg: "w-[353px] h-[57px] text-[20px]",
   };
 
   const variantStyles = {
     primary: "bg-[var(--color-primary)] text-white",
-    secondary: "bg-[var(--color-gray-200)] text-black",
+    gray200: "bg-[var(--color-gray-200)] text-black",
+    gray600: "bg-[var(--color-gray-600)] text-[var(--color-gray-400)]",
   };
 
   return (
