d8e957f1e66cb32e8b3670c601867a6eb5929830
shinwokkang <tlsdnr303@gmail.com>
2025-08-04 15:08:20 +0900

    feat: 스토어의 mode를 감지하여 두 가지 상태("일반"과 "다시 만들기")를 처리하도록 구현

--- src/components/registration/avatarCreation/AvatarCreationOption.tsx
+++ src/components/registration/avatarCreation/AvatarCreationOption.tsx
@@ -2,7 +2,6 @@
  * @file AvatarCreationOption.tsx
  * @description 아바타 생성 페이지의 옵션 컴포넌트
  */
-
 import React from "react";
 
 import { useNavigate } from "react-router-dom";
@@ -10,34 +9,58 @@ import { useNavigate } from "react-router-dom";
 import Button from "@/components/common/Button";
 import { useAvatarCreationStore } from "@/stores/avatarCreationStore";
 
-const AvatarCreationOption = () => {
-  const navigate = useNavigate();
-  const { buttonVariant, buttonText } = useAvatarCreationStore();
+interface AvatarCreationOptionProps {
+  optionId: string;
+}
 
-  const handleNavigate = () => {
+const AvatarCreationOption: React.FC<AvatarCreationOptionProps> = ({
+  optionId,
+}) => {
+  const navigate = useNavigate();
+  const { mode, selectedOptionId, actions } = useAvatarCreationStore();
+
+  const isSelected = selectedOptionId === optionId;
+
+  // 컨테이너 클릭 핸들러: 'remake' 모드일 때만 클릭 이벤트를 처리함.
+  const handleContainerClick = () => {
+    if (mode === "remake") {
+      actions.selectOption(optionId);
+    }
+  };
+
+  // 버튼 클릭 핸들러: 항상 페이지를 이동시키도록 함.
+  const handleButtonClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
     navigate("/registration/creation-detail");
   };
 
   return (
-    <div className="flex items-center justify-between h-full">
+    <div
+      className={`flex items-center justify-between h-full  ${
+        isSelected
+          ? "bg-[var(--color-primary-varient)] border-none"
+          : "border-transparent"
+      } ${mode === "remake" ? "cursor-pointer" : "cursor-default"}`}
+      onClick={handleContainerClick}
+    >
       <div className="flex flex-col justify-between pl-6.25 h-full">
         <div className="flex flex-col gap-3 w-41.25 text-left">
           <h2 className="text-2xl font-semibold pt-8">나만의 아바타</h2>
-          <p className="text-lg ">
+          <p className="text-lg">
             내 식물의 생김새를 반영한 나만의 아바타를 만들 수 있어요
           </p>
         </div>
         <div className="pb-10.25">
           <Button
-            variant={buttonVariant}
+            variant={mode === "initial" ? "primary" : "gray200"}
             size="xsCreation"
-            onClick={handleNavigate}
+            onClick={handleButtonClick}
           >
-            {buttonText}
+            {mode === "initial" ? "만들러 가기" : "다시 만들기"}
           </Button>
         </div>
       </div>
-      <div className="w-1/2 h-full "></div>
+      <div className="w-1/2 h-full"></div>
     </div>
   );
 };
