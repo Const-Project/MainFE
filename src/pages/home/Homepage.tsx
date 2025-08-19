b3d0c98af85574646e72278089c34c64ef2a18f2
leejuhi <anim0203@naver.com>
2025-08-19 14:55:29 +0900

    feat: 바텀 시트

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -1,24 +1,35 @@
 import { useState } from "react";
 
+import BottomSheet from "@/components/bottom-sheet/BottomSheet";
 import Lock from "@/components/lock/Lock";
 import UnLock from "@/components/lock/UnLock";
 
+type BottomSheetType = "lock" | "unlock" | "clear";
+
 const HomePage = () => {
-  const [isUnlocked, setIsUnlocked] = useState(false);
+  const [isUnlocked, setIsUnlocked] = useState<BottomSheetType>("lock");
   return (
-    <div className="flex w-full items-center justify-center h-full ">
+    <div className="relative flex w-full items-center justify-center h-full ">
       <div className="flex flex-col h-full items-center justify-between w-full">
-        <div className="flex-1 flex items-center justify-center">
-          {isUnlocked ? <UnLock /> : <Lock />}
-        </div>
-        <button
-          onClick={() => setIsUnlocked(!isUnlocked)}
-          className={`w-full ${isUnlocked ? "bg-primary text-white" : "bg-gray-200 text-gray-400"} h-16 text-heading2`}
-        >
-          {!isUnlocked
-            ? "아직 감자가 충분히 모이지 않았어요"
-            : "씨앗 받고 해금하기!"}
-        </button>
+        {isUnlocked !== "clear" && (
+          <>
+            <div className="flex-1 flex items-center justify-center">
+              {isUnlocked === "unlock" ? <UnLock /> : <Lock />}
+            </div>
+
+            <button
+              onClick={() =>
+                setIsUnlocked(isUnlocked === "lock" ? "unlock" : "clear")
+              }
+              className={`w-full ${isUnlocked === "unlock" ? "bg-primary text-white" : "bg-gray-200 text-gray-400"} h-16 text-heading2`}
+            >
+              {isUnlocked === "lock"
+                ? "아직 감자가 충분히 모이지 않았어요"
+                : "씨앗 받고 해금하기!"}
+            </button>
+          </>
+        )}
+        {isUnlocked === "clear" && <BottomSheet />}
       </div>
     </div>
   );
