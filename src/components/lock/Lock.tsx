c8318616c4bea207bfbc8927f0358664cd341007
leejuhi <anim0203@naver.com>
2025-08-20 12:50:33 +0900

    chore: 리뷰 반영

--- src/components/lock/Lock.tsx
+++ src/components/lock/Lock.tsx
@@ -5,7 +5,7 @@ const Lock = () => {
     <div className="flex flex-col items-center justify-center gap-2">
       <LockIcon className="w-8 h-8" />
       <div className="text-heading2 text-danger">아직 해금되지 않았습니다!</div>
-      <div className="text-body2 text-gray-600 text-center">
+      <div className="text-body2 text-black text-center">
         감자를 충분히 모은 뒤에 <br />
         새로운 식물을 키울 수 있어요.
       </div>
